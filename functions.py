c934d8bcd75ce88df8b6061dbd6d8e7e81c0838d
Cristian Monsalve <cdmonsalvea1@gmail.com>
2023-03-05 12:18:17 -0500

    v 1.1

---
+++ functions.py
@@ -0,0 +1,6 @@
+# Info: This file contains all the functions used in the main file for processing the solutions
+# Funtions for getting data from the database, and for plotting the data
+
+
+#### Funciones necesarias ####
+
